88bad02873727b22181d2255968e07c70818785e
Tushar Gupta <tushargupta4870@gmail.com>
2024-04-14 17:12:17 +0530

    basic folder struture, and  server made

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+const port = 3000
+
+const app = express();
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port} `)
+})
+
